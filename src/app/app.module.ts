449bec6ff2e55df0b1a396245892267fc145211c
Asad Riaz <dev.asadriaz@gmail.com>
2019-03-23 01:39:52 +0500

    accuracy and time out added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -16,6 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    NativeGeocoder,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
-
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+
+  responseObj: any;
+  watchLocationUpdates: any;
+  isWatching: boolean;
+  latitude: any;
+  longitude: any;
+  altitude: any;
+  accuracy: any;
+  altAccuracy: any;
+  heading: any;
+  speed: any;
+  timestamp: any;
+
+  constructor(private geolocation: Geolocation, private platform: Platform) { }
+
+
+ 
+
+  getCurrentPosition() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+      this.altitude = resp.coords.altitude;
+      this.accuracy = resp.coords.accuracy;
+      this.altAccuracy = resp.coords.altitudeAccuracy;
+      this.heading = resp.coords.heading;
+      this.speed = resp.coords.speed;
+      this.timestamp = new Date(resp.timestamp);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+  }
+ 
+  //Start location update watch
+  watchLocation() {
+
+
+    this.isWatching = true;
+    this.watchLocationUpdates = this.geolocation.watchPosition({ maximumAge: 60000, timeout: 25000, enableHighAccuracy: true})
+                                 .subscribe(resp => {
+                                  this.latitude = resp.coords.latitude;
+                                  this.longitude = resp.coords.longitude;
+                                  this.altitude = resp.coords.altitude;
+                                  this.accuracy = resp.coords.accuracy;
+                                  this.altAccuracy = resp.coords.altitudeAccuracy;
+                                  this.heading = resp.coords.heading;
+                                  this.speed = resp.coords.speed;
+                                  this.timestamp = new Date(resp.timestamp);
+     });
+
+
+  }
+
+  //Stop location update watch
+  stopLocationWatch() {
+    alert("hogayea");
+    this.isWatching = false;
+    this.watchLocationUpdates.unsubscribe();
+   
+  }
+
 
 }
